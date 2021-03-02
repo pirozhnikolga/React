3dffdf66ec3437a743a2fe4d7e30cc880df71eb9
pirozhenka <ipirozhenka@mail.ru>
2021-03-02 21:43:01 +0300

    Рефакторинг копирования объекта

--- Projects/01/testing01/src/redux/dialogsReducer.js
+++ Projects/01/testing01/src/redux/dialogsReducer.js
@@ -20,11 +20,10 @@ let initialState = {
 const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_TEXT: {
-
-            let stateCopy = { ...state };
-            stateCopy.newMessageText = action.text;
-
-            return stateCopy;
+            return {
+                ...state,
+                newMessageText: action.text
+            };
         }
         case ADD_MESSAGE: {
 
@@ -37,12 +36,11 @@ const dialogsReducer = (state = initialState, action) => {
                 text: state.newMessageText
             };
 
-            let stateCopy = { ...state };
-            stateCopy.messages = [...state.messages];
-            stateCopy.messages.push(newMessage);
-            stateCopy.newMessageText = "";
-
-            return stateCopy;
+            return {
+                ...state,
+                messages: [...state.messages, newMessage],
+                newMessageText: ""
+            };
         }
         default: return state;
     }
--- Projects/01/testing01/src/redux/profileReducer.js
+++ Projects/01/testing01/src/redux/profileReducer.js
@@ -25,19 +25,17 @@ const profileReducer = (state = initialState, action) => {
                 likesCount: 0
             };
 
-            let stateCopy = { ...state };
-            stateCopy.posts = [ ...state.posts ];
-            stateCopy.posts.push(newPost);
-            stateCopy.newPostText = "";
-
-            return stateCopy;
+            return {
+                ...state,
+                posts: [...state.posts, newPost],
+                newPostText: ""
+            };
         }
         case UPDATE_NEW_POST_MESSAGE: {
-
-            let stateCopy = { ...state };
-            stateCopy.newPostText = action.text;
-
-            return stateCopy;
+            return {
+                ...state,
+                newPostText: action.text
+            };
         }
         default: return state;
     }
